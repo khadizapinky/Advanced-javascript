d66bea9ac7b369b6ad9e93939678beab99ae4a41
khadizapinky <khadizapinky303@gmail.com>
2020-11-14 23:17:28 +0600

    first

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//false:
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+
+//True
+//'0', ' ' ,[], {},'false'
+
+// const name  = "solaiman";
+const name = "";
+
+if(name || name == 0){
+    console.log('condition is ture');
+}
+else{
+    console.log('condition is false');
+}
